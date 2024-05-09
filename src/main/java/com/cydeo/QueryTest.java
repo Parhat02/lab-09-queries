1e32e99966ed76ac5726e23b98bb3f30faec325a
Ferhat <parhat002@gmail.com>
2024-05-09 22:22:46 +0300

    BalanceRepository

--- src/main/java/com/cydeo/QueryTest.java
+++ src/main/java/com/cydeo/QueryTest.java
@@ -4,6 +4,8 @@ import com.cydeo.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class QueryTest implements CommandLineRunner {
 
@@ -42,6 +44,13 @@ public class QueryTest implements CommandLineRunner {
         System.out.println("Address Of The With Customer Id : " + addressRepository.getAllAddresses(3));
         System.out.println("Address : " + addressRepository.findTop3ByNameStartingWith("Home"));
         System.out.println("Top 3 Address of that email: " + addressRepository.findDistinctTop3ByCustomerEmail("elawie26@exblog.jp"));
+
+        System.out.println("Top 5 Balance: " + balanceRepository.findDistinctTop5ByAmountIsNotNullOrderByAmountDesc());
+        System.out.println("Balance: " + balanceRepository.findByAmountGreaterThanEqual(BigDecimal.valueOf(980)));
+        System.out.println("Balance: " + balanceRepository.findByAmountLessThanEqual(BigDecimal.valueOf(50)));
+
+
+
 
     }
 }
--- src/main/java/com/cydeo/repository/BalanceRepository.java
+++ src/main/java/com/cydeo/repository/BalanceRepository.java
@@ -2,12 +2,24 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
 
     //Write a native query to get top 5 max balance
+    @Query(value = "SELECT * FROM Balance Where amount is not null order by amount desc limit 5", nativeQuery = true)
+    //@Query("SELECT b FROM Balance b where b.amount is not null order by b.amount desc limit 5")
+    List<Balance> findDistinctTop5ByAmountIsNotNullOrderByAmountDesc();
 
     //Write a derived query to get all balances greater than or equal to specific balance amount
+    //@Query("SELECT b from Balance b where b.amount >= ?1")
+    //@Query(value = "SELECT * from balance where amount >= ?1", nativeQuery = true)
+    List<Balance> findByAmountGreaterThanEqual(BigDecimal amount);
 
     //Write a native query to get all balances less than specific balance amount
+    @Query(value = "SELECT * from balance where amount <= ?1", nativeQuery = true)
+    List<Balance> findByAmountLessThanEqual(BigDecimal amount);
 }
